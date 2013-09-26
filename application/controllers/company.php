6d4d16aa552acd893eb7ba4ac16f9c5a1224062a
david <david@sinodev.com>
2013-09-26 13:52:58 +0800

    Add New validate.js.  Change select to origin function way.

--- application/controllers/company.php
+++ application/controllers/company.php
@@ -40,4 +40,34 @@ class company extends Front_Controller {
         $data = $this->data;
         $this->load->view($data['front_theme']."/company-profile",$data);
     }
+
+    /**
+     *  update user's photo
+     */
+    public function ajaxuploadimage() {
+        $data = $this->data;
+
+        // create folder
+        $this->load->model('jobseeker_model');
+        $user_path = realpath(dirname(__FILE__))."/../../theme/default/company/";
+        $this->jobseeker_model->creatUserfolder ( $user_path ) or exit ( 'error: can not create folder.' );
+        // upload
+        if (is_uploaded_file ( $_FILES ['avatar'] ['tmp_name'] )) {
+            $file_name = iconv('utf-8','gb2312',$_FILES['avatar']['name']);
+
+            $i = 1;
+            while (file_exists($user_path .$file_name)) {
+            	$file_name = $i++."_".$file_name;
+            }
+            
+            move_uploaded_file ( $_FILES ['avatar'] ['tmp_name'], $user_path .$file_name);
+
+            // new image url
+            $data ['avatar'] = '/company/' . $file_name;
+
+            exit ( 'success' );
+        } else {
+            exit ( 'error: can not upload avatar image.' );
+        }
+    }
 }
