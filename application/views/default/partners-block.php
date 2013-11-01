d1df08e8fa936896bc22d9f309b0e893565b2574
Zhang Cheng <mimizzee@126.com>
2013-11-01 16:19:13 +0800

    update salary and partner

--- application/views/default/partners-block.php
+++ application/views/default/partners-block.php
@@ -2,7 +2,7 @@
 		<div class="w770">
 			<div class="title">Our Partners</div>
 			<div class="h-partners-bd">
-				<div class="p-partners-item"><img src="<?php echo $theme_path?>style/home/temp/pertner-rs.png"  alt="" /></div>
+				<div class="p-partners-item"><img src="<?php echo $theme_path?>style/home/temp/pertner-hb.png"  alt="" /></div>
 				<div class="p-partners-item"><img src="<?php echo $theme_path?>style/home/temp/pertner-ca.png"  alt="" /></div>
 				<div class="p-partners-item"><img src="<?php echo $theme_path?>style/home/temp/pertner-hds.png"  alt="" /></div>
 				<div class="p-partners-item"><img src="<?php echo $theme_path?>style/home/temp/pertner-bc.png"  alt="" /></div>
