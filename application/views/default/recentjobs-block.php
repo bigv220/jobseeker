5b296e070a9b1e122126b7ba68086b07172c0ed8
Andrew Wang <andrew@kooeu.com>
2013-11-21 09:30:21 +0800

    New wider size and square logos

--- application/views/default/recentjobs-block.php
+++ application/views/default/recentjobs-block.php
@@ -6,8 +6,7 @@
 			<div class="w770">
 			<?php foreach ($recently_jobs as $arr):?>
 				<a class="p-jobs-item rel" href="<?php echo $site_url.'job/jobDetails/'.$arr['id']?>">
-					<i class="mark abs"></i>
-			      	<img src="<?php echo $site_url.'attached/users/'.$arr['profile_pic']?>" width="75px;" />
+			      	<img src="<?php echo $site_url.'attached/users/'.$arr['profile_pic']?>" width="55px;" />
 			      	<div class="jobtitle abs"><?php echo $arr['job_name']?></div>
 			      	<div class="city abs"><?php echo $arr['city']?></div>
 		      	</a>
