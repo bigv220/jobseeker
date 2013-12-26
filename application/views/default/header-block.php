0729a0dc502ee0a7733818aaf110f59f77d04cf4
Zhang Cheng <mimizzee@126.com>
2013-12-26 14:24:37 +0800

    take 'index' off from dropdown menu

--- application/views/default/header-block.php
+++ application/views/default/header-block.php
@@ -116,7 +116,7 @@
                             <li><a href="<?php echo $site_url?>company/joblisting">Manage Job Listings</a></li>
                             <li><a href="<?php echo $site_url?>company/applicants">View Applicants</a></li>
                             <li><a href="<?php echo $site_url?>company/shortlistCandidates">View Shortlisted Candidates</a></li>
-                            <li><a href="<?php echo $site_url?>inbox">JINGCHAT Inbox</a></li>
+                            <li><a href="<?php echo $site_url?>inbox">JINGCHAT</a></li>
 
                         <?php endif;?>
                         <li class="last"><a href="<?php echo $site_url?>user/logout">Sign out</a></li>
