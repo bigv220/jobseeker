d09601d3ae9afc767f174901db135af29a999549
Andrew Wang <andrew@kooeu.com>
2014-01-02 16:28:27 +0800

    implement static page of JingNews_All_top_stories.psd

--- application/views/default/top-stories.php
+++ application/views/default/top-stories.php
@@ -4,61 +4,78 @@
 
         <div class="news-index news_details box rel mb10">
             <div class="news_top_header">
-                <div class="title">Russia & China To Start Youth Exchange Program</div>
-                <div class="subtitle">As the world looks to China in awe, the middle kingdom itself is strengthening ties with its immediate neighbours. 
-                </div>
-                <div class="sns_share_btns">
-                    <a href="#"><img src="<?php echo $theme_path;?>/style/btns/btn_like.png" alt=""/></a>
-                    <a href="#"><img src="<?php echo $theme_path;?>/style/btns/btn_share.png" alt=""/></a>
-                </div>
+                <div class="title">Top Stories on JingNews</div>
+
             </div>
             <div class="news-index-left">
-                <div class="article">
-                    <div class="content">
-                        <img src="<?php echo $theme_path?>style/home/article_img.jpg"/>
-                        <p>
-                        As the world looks to China in awe, the middle kingdom itself is strengthening ties with its immediate neighbours. <br />
-						In fact, Russia and China will send 10,000 students to each other’s countries, to strengthen international relations as part of the Youth Friendly Exchanges Year. <br />
-						Chinese Vice Premier Liu Yandong said:<br />
-						"We will launch a youth exchange program that involves 100 schools and 10,000 students from each country.<br />
-						"We will earnestly plan and prepare for the opening ceremony of the youth year to be held in Russia next spring...<br />
-                            "We will earnestly plan and prepare for the opening ceremony of the youth year to be held in Russia next spring
-                            <br />
-                            Liu feels there is an opportunity to increase friendly cooperation and further the China-Russia strategic partnership. 
-                            <br />
-                            In addition, one million students will be able to engage online with the hope of increasing the numbers of Chinese-Russian translation students. 
-                            <br />
-                            Furthermore, the two countries will work together at the Sochi Winter Olympic Games and send young athletes to the sixth China-Russia Youth Games as well. 
-                            <br />
-                            Other countries including India and the UK are looking at forming relationships with China, undoubtedly highlighting the global influence the country has. Of course the communist history shared with Russia and China will play a large part in the ways they move forward socially, economically and politically. 
-                        </p>
+                <div class="article_row">
+                    <div class="article_thumbnail">
+                        <img src="<?php echo $theme_path;?>style/search/job-img1.gif" alt="" width="75px" height="75px" class="round_img"/>
                     </div>
-                    <!-- <div class="share-sns">
-                        <a href="#" class="like_btn">Like</a>
-                        <a href="#" class="share_btn">Share</a>
-                    </div> -->
+                    <div class="article_abstract">
+                        <p class="title">Shanghai Free Trade Zone to Welcome Hotel Zoom</p>
+                        <p class="post_date">November 20th 2013</p>
+                        <p class="content_abstract">The launch of the Shanghai Free Trade Zone will be a massive step forward for the city and experts expect a knock-on effect, of increased hotel bookings.</p>
+                        <a href="#">Continue Reading</a>
+                    </div>
+                    <div style="clear:both;"></div>
+                </div>
+
+                <div class="article_row">
+                    <div class="article_thumbnail">
+                        <img src="<?php echo $theme_path;?>style/search/job-img1.gif" alt="" width="75px" height="75px" class="round_img"/>
+                    </div>
+                    <div class="article_abstract">
+                        <p class="title">Shanghai Free Trade Zone to Welcome Hotel Zoom</p>
+                        <p class="post_date">November 20th 2013</p>
+                        <p class="content_abstract">The launch of the Shanghai Free Trade Zone will be a massive step forward for the city and experts expect a knock-on effect, of increased hotel bookings.</p>
+                        <a href="#">Continue Reading</a>
+                    </div>
+                    <div style="clear:both;"></div>
+                </div>
+
+                <div class="article_row">
+                    <div class="article_thumbnail">
+                        <img src="<?php echo $theme_path;?>style/search/job-img1.gif" alt="" width="75px" height="75px" class="round_img"/>
+                    </div>
+                    <div class="article_abstract">
+                        <p class="title">Shanghai Free Trade Zone to Welcome Hotel Zoom</p>
+                        <p class="post_date">November 20th 2013</p>
+                        <p class="content_abstract">The launch of the Shanghai Free Trade Zone will be a massive step forward for the city and experts expect a knock-on effect, of increased hotel bookings.</p>
+                        <a href="#">Continue Reading</a>
+                    </div>
+                    <div style="clear:both;"></div>
+                </div>
+
+                <div class="article_row">
+                    <div class="article_thumbnail">
+                        <img src="<?php echo $theme_path;?>style/search/job-img1.gif" alt="" width="75px" height="75px" class="round_img"/>
+                    </div>
+                    <div class="article_abstract">
+                        <p class="title">Shanghai Free Trade Zone to Welcome Hotel Zoom</p>
+                        <p class="post_date">November 20th 2013</p>
+                        <p class="content_abstract">The launch of the Shanghai Free Trade Zone will be a massive step forward for the city and experts expect a knock-on effect, of increased hotel bookings.</p>
+                        <a href="#">Continue Reading</a>
+                    </div>
+                    <div style="clear:both;"></div>
                 </div>
 
 
             </div>
             <div class="news-index-right">
-                <div class="read_more_stories">
-                    <div class="title">Read More Stories</div>
-                    <ul>
-                        <li><a href="http://gradrecruiter.wordpress.com/2013/08/29/is-your-mind-set-to-global/">7 ways to improve your global mindset</a></li>
-                        <li><a href="http://www.theatlantic.com/infocus/2013/09/scenes-from-21st-century-china/100586/">Scenes from 21st Century China</a></li>
-                        <li><a href="<?php echo $site_url?>news/view/169">Shanghai Free Trade Zone to welcome hotel boom</a></li>
-                    </ul>
-                </div>
+                <div class="filter_top_stories_title">Filter Top Stories</div>
+                Search
+                <form action="<?php echo $site_url; ?>" method="post" id="search_top_stories_form">
+                    <input type="text" name="keywords" class="abs input-tip search_top_stories_input" value="Search by Keywords" data-tipval="Search by Keywords"/>
+                    <input type="submit" class="abs search_top_stories_btn" value=""  title="search"/>
+                </form>
+                <div>View Stories From</div>
+                <select name="stories_from" required>
+                    <option value="">Last 30 Days</option>
+                    <option value="">Last 90 Days</option>
+                    <option value="">Last 180 Days</option>
+                </select>
 
-                <div class="read_more_stories">
-                    <div class="title">Useful Links & Things We Like</div>
-                    <ul>
-                        <li><a href="http://gradrecruiter.wordpress.com/2013/08/29/is-your-mind-set-to-global/">7 ways to improve your global mindset</a></li>
-                        <li><a href="http://www.theatlantic.com/infocus/2013/09/scenes-from-21st-century-china/100586/">Scenes from 21st Century China</a></li>
-                        <li><a href="<?php echo $site_url?>news/view/169">Shanghai Free Trade Zone to welcome hotel boom</a></li>
-                    </ul>
-                </div>
 
             </div>
             <div class="clearfix"></div>
